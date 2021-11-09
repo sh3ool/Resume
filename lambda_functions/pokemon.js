0a4bb346243fe84d723bc337ba40ef8e3309bbf8
Meshal <shaool1991@gmail.com>
2021-11-09 13:03:39 +0300

    update pok

--- lambda_functions/pokemon.js
+++ lambda_functions/pokemon.js
@@ -33,35 +33,11 @@ const queryDatabase = async (db) => {
   };
 };
 
-// ./lambda_functions/pokemon.js
-
-const pushToDatabase = async (db, data) => {
-  const pokemonData = {
-    name: data.name,
-    number: data.number,
-  };
-
-  if (pokemonData.name && pokemonData.number) {
-    await db.collection("pokemon").insertMany([data]);
-    return { statusCode: 201 };
-  } else {
-    return { statusCode: 422 };
-  }
-};
-
 module.exports.handler = async (event, context) => {
   // otherwise the connection will never complete, since
   // we keep the DB connection alive
   context.callbackWaitsForEmptyEventLoop = false;
 
   const db = await connectToDatabase(MONGODB_URI);
-  
-  switch (event.httpMethod) {
-    case "GET":
-      return queryDatabase(db);
-    case "POST":
-      return pushToDatabase(db, JSON.parse(event.body));
-    default:
-      return { statusCode: 400 };
-  }
+  return queryDatabase(db);
 };
