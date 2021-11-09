b20ff098825e3f593350363631bf1cb18ad54d23
Meshal <shaool1991@gmail.com>
2021-11-09 13:15:15 +0300

    test db connection

---
+++ lambda_functions/test.js
@@ -0,0 +1,16 @@
+// test script
+
+const postRequest = await fetch("/.netlify/functions/pokemon", {
+    method: "POST",
+    body: JSON.stringify({
+      name: "Pikachu",
+      number: 25,
+    }),
+  });
+  
+  console.log("POST request status code", postRequest.status);
+  
+  const newGetRequest = await fetch("/.netlify/functions/pokemon");
+  const newListJson = await newGetRequest.json();
+  
+  console.log("GET request new result", newListJson);
