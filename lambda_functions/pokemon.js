c78d648bab8efb73cfb2ab91d7db2974c923566b
Meshal <shaool1991@gmail.com>
2021-11-09 12:50:39 +0300

    new DB connection

---
+++ lambda_functions/pokemon.js
@@ -0,0 +1,67 @@
+// ./lambda_functions/pokemon.js
+
+const MongoClient = require("mongodb").MongoClient;
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const DB_NAME = 'test_DB';
+
+let cachedDb = null;
+
+const connectToDatabase = async (uri) => {
+  // we can cache the access to our database to speed things up a bit
+  // (this is the only thing that is safe to cache here)
+  if (cachedDb) return cachedDb;
+
+  const client = await MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+  });
+
+  cachedDb = client.db(DB_NAME);
+
+  return cachedDb;
+};
+
+const queryDatabase = async (db) => {
+  const pokemon = await db.collection("pokemon").find({}).toArray();
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(pokemon),
+  };
+};
+
+// ./lambda_functions/pokemon.js
+
+const pushToDatabase = async (db, data) => {
+  const pokemonData = {
+    name: data.name,
+    number: data.number,
+  };
+
+  if (pokemonData.name && pokemonData.number) {
+    await db.collection("pokemon").insertMany([data]);
+    return { statusCode: 201 };
+  } else {
+    return { statusCode: 422 };
+  }
+};
+
+module.exports.handler = async (event, context) => {
+  // otherwise the connection will never complete, since
+  // we keep the DB connection alive
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const db = await connectToDatabase(MONGODB_URI);
+  
+  switch (event.httpMethod) {
+    case "GET":
+      return queryDatabase(db);
+    case "POST":
+      return pushToDatabase(db, JSON.parse(event.body));
+    default:
+      return { statusCode: 400 };
+  }
+};
