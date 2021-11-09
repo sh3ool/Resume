daa7ac4b74e1207c819bbec2b0ed27bcdb66b408
Meshal <shaool1991@gmail.com>
2021-11-09 13:23:21 +0300

    update test

--- lambda_functions/test.js
+++ lambda_functions/test.js
@@ -9,8 +9,3 @@ const postRequest = await fetch("/.netlify/functions/pokemon", {
   });
   
   console.log("POST request status code", postRequest.status);
-  
-  const newGetRequest = await fetch("/.netlify/functions/pokemon");
-  const newListJson = await newGetRequest.json();
-  
-  console.log("GET request new result", newListJson);
